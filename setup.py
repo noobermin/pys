0be3b34cf0e394cd7ff7a8844120ea29fa02d00e
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2017-07-17 13:13:26 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pys',
-      version='0.0.8r5',
+      version='0.0.9',
       description='Miscellaneous Pythons',
       url='http://github.com/noobermin/pys',
       author='noobermin',
